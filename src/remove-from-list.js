98be2683087e4544c47698b12de7b0c6afd41356
Evgeniia Zhavoronkova <maongaku@gmail.com>
2023-12-19 10:31:03 +0100

    feat: add queue and remove  task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  let list = l;
+  let lastItem = undefined;
+
+  while(list != undefined) {
+    if (list.value == k) {
+      if(lastItem != undefined) {
+        lastItem.next = list.next
+      } else {
+        l = list.next;
+      }
+      list = l;
+    }
+    lastItem = list;
+    list = list.next;
+  }
+  return l;
 }
 
 module.exports = {
